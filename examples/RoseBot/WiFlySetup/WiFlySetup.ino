045e7235ad65a37953d5f06f417d526d9625c5a1
David Fisher <fisherds@rose-hulman.edu>
2015-10-03 00:03:54 -0400

    minor cleanup and formatting

--- examples/RoseBot/WiFlySetup/WiFlySetup.ino
+++ examples/RoseBot/WiFlySetup/WiFlySetup.ino
@@ -8,37 +8,28 @@
  * RX is digital pin 15 (connect to TX of other device)
  * TX is digital pin 14 (connect to RX of other device)
 
-
  created back in the mists of time
  modified 25 May 2012
  by Tom Igoe
  based on Mikal Hart's example
 
  This example code is in the public domain.
-
  */
+
 #include <SoftwareSerial.h>
 
 SoftwareSerial WiflySerial(15, 14); // RX, TX
 
-void setup()
-{
-  // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-  // set the data rate for the SoftwareSerial port
-  WiflySerial.begin(115200);
-  WiflySerial.println("Hello, world?");
+void setup() {
+  Serial.begin(115200);  // Baudrate used in the Serial Monitor window (can be anything)
+  WiflySerial.begin(57600);  // Baudrate used to talk to WiFly (keep setting that you plan to use with Firmata)
 }
 
-void loop() // run over and over
-{
-  if (WiflySerial.available())
+void loop() {
+  if (WiflySerial.available()) {
     Serial.write(WiflySerial.read());
-  if (Serial.available())
+  }
+  if (Serial.available()) {
     WiflySerial.write(Serial.read());
+  }
 }
-
